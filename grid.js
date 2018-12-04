869c1f60fee65607d6a692244202870f71cf1941
Ph0enixKM <pkaras.it@gmail.com>
2018-12-04 22:29:49 +0100

    Grid fix & backgrund change

--- grid.js
+++ grid.js
@@ -4,23 +4,28 @@ let canvas = qs('jumbo .bg')
 let c = canvas.getContext('2d')
 const PI = Math.PI
 let size = canvas.getBoundingClientRect()
-var { width, height, top, left } = size
+let { width, height } = size
+let canTop = size.top;
+let canLeft = size.left
 canvas.width = width
 canvas.height = height
 let x = 0
 let y = 0
 
-function update(c, canvas){
-
+function update(c, canvas, x, y){
+    c.clearRect(0, 0, width, height)
     c.fillStyle = 'rgba(0,0,0,0.3)'
     c.fillRect(0, 0, width, height)
 
     for (let i = 0; i < width/50; i++){
         for (let j = 0; j < height/50; j++){
-            let vw = Math.pow(x - i*50,2)
-            let vh = Math.pow(y - j*50,2)
+            let vw = Math.pow(x - i*50 - canLeft, 2)
+            let vh = Math.pow(y - j*50 - canTop, 2)
             let wh = Math.sqrt(vw + vh)
-            let res = (wh < 50) ? (wh+1)/50 : 1
+            let res = (wh < 150) ? 150/(wh+1) : 1
+            res = (res > 5) ? 5 : res
+            
+
         
             c.fillStyle = 'white'
             c.beginPath()
@@ -33,6 +38,6 @@ function update(c, canvas){
 
 window.addEventListener('mousemove', e => {
     let { x, y } = e
-    update(c, canvas)
+    update(c, canvas, x, y)
 })
-update(c, canvas)
+update(c, canvas, x, y)
