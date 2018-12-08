b78f53fb94bfecda0d3fbe459f44c9725875ecb9
Ph0enixKM <pkaras.it@gmail.com>
2018-12-08 20:11:26 +0100

    Labels

--- scripts/appear.js
+++ scripts/appear.js
@@ -1,7 +1,27 @@
 function appear (qs) {
-    let element = document.querySelector(qs)
-    let offset = element.getBoundingClientRect().top
-    
+    let elements = document.querySelectorAll(qs)
+
+    for (const el of elements) {
+        let offset = el.getBoundingClientRect().top
+
+        setInterval(() => {   
+            let cont = document.querySelector('cont').scrollTop
+            console.log(offset);
+            
+
+            if (cont >= offset - 50) {
+                el.children[1].style.transform = 'translate(0, -100%)'
+            } else {
+                el.children[1].style.transform = 'translate(-200%, -100%)'
+            }
+            
+        }, 100)
+
+        el.onclick = () => {
+            location.href = 'working.html'
+        }
+        
+    }
     
 
 }
